a942acdde3a7bc1dcf13dce252255c21ad9784e5
Shivam30Mishra <theshivammishra10@gmail.com>
2026-01-09 19:36:57 +0530

    understood the layout in next js and the file based routing in next js

---
+++ app/blog/page.tsx
@@ -0,0 +1,33 @@
+import axios from "axios"
+
+async function getBlog() {
+  const response = await axios.get("https://jsonplaceholder.typicode.com/todos")
+  return response.data;
+}
+
+export default async function blog() {
+  const blogs = await getBlog();
+  return(
+    <>
+      This is the blog page
+      {blogs.map((blog : any) => (
+        <Todo title={blog.title} completed={blog.completed} />
+      ))}
+    </>
+  )
+}
+
+interface ITodo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+
+function Todo ({ title,completed } : ITodo) {
+  return(
+    <p>
+    {title} {completed ? "done!" : "not done"}
+    </p>
+  )
+}
---
+++ app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function SignIn() {
+  return(
+    <>
+      This is the sign up page
+    </>
+  )
+}
