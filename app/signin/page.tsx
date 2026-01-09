53e6fd513725d7b3277744f7f153d071efeedd2f
Shivam30Mishra <theshivammishra10@gmail.com>
2026-01-10 00:49:39 +0530

    understood how using the useEffect useState detioroites the capability of next js by applying waterfalling problem same as in react and not using the next js ability

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,7 +1,33 @@
-export default function SignIn() {
+"use client"
+
+import { useEffect,useState } from 'react'
+import axios from 'axios'
+
+export default function Sign() {
+  const [ loading,setLoading ] = useState(true)
+  const [ data,setData ] = useState({})
+
+  useEffect(()=>{
+    axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+      .then(response =>{
+        setData(response.data)
+        setLoading(false)
+      })
+  },[])
+
+  if(loading){
+    return (
+      <>
+        loading
+      </>
+    )
+  }
+
   return(
     <>
-      This is the sign in page
+      {data?.name}
+      {data?.email}
     </>
   )
+
 }
